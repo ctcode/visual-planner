ebf055a457d93b6f66a2f5e321f12ea8336a7bcb
ct <ct6787@gmail.com>
2017-10-30 11:09:39 +0000

    dev

--- beta/vip_analytics.js
+++ beta/vip_analytics.js
@@ -4,7 +4,8 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-60115120-4', 'auto');
-ga('send', 'pageview', {'sessionControl': 'start'});
+ga('send', 'pageview');
+//ga('send', 'pageview', {'sessionControl': 'start'});
 
 function ga_hit(category, action)
 {ga('send', 'event', {'eventCategory': category, 'eventAction': action.toString()});}
