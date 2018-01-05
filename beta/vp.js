09966dc79a83dcb5635bc31aaa28c17e4a91448c
ct <ct6787@gmail.com>
2018-01-05 00:17:23 +0000

    dev

--- beta/vp.js
+++ beta/vp.js
@@ -1,7 +1,7 @@
 function mainCtrl($scope)
 {
 	var gAccount = new AuthAccount();
-	gAccount.authClientID = '304094492573-vgmnc6pr5tf1va809qpoc6sri6amiptk.apps.googleusercontent.com';
+	gAccount.authClientID = '304094492573-vgmnc6pr5tf1va809qpoc6sri6amiptk.apps.googleusercontent.com';  // vp
 	gAccount.authScope = 'https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/drive.appdata';
 
 	var gAppData = new AuthAppData();
